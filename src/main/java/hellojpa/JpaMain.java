3e6a8c5e91cf82466656fb200b820878200582fd
yeolmu96 <jeong7653@naver.com>
2025-07-10 11:20:48 +0900

    first commit message

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,26 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Member
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+        emf.close();
+
+    }
+}
